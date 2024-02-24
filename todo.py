e94a104df6634cc4c6245c6e29e68b8fa8688684
Veer <veer@example.com>
2024-02-24 13:51:31 -0500

    added comments

--- todo.py
+++ todo.py
@@ -2,23 +2,30 @@
 #A list variable for storing the tasks from the user.
 tasks = []
 
+#Functions
+#adds a task to the task list with the "append" method, by taking the users input for the task description.
 def add_task(description):
     tasks.append({"description": description, "completed": False})
 
+#Asks user for index of task they want to set to True for "Completion"
 def complete_task(task_index):
     tasks[task_index]["completed"] = True
 
+#This line is asking the index of task that they want to remove.
 def remove_task(task_index):
     del tasks[task_index]
 
+#This function sets the completion propertyof all tasks to false.
 def reset_tasks():
     for task in tasks:
         task["completed"] = False
 
+#Mr.Sabo wrote this code 
 def save_tasks(filename):
     with open(filename, "w") as f:
         for task in tasks:
             f.write(task["description"] + "," + str(task["completed"]) + "\n")
+
 
 def load_tasks(filename):
     tasks.clear()
@@ -41,6 +48,7 @@ def main():
 
         choice = input("Enter your choice (add/complete/remove/reset/quit): ").lower()
 
+        #takes users input and determines which function gets called based on the input
         if choice == "add":
             description = input("Enter task description: ")
             add_task(description)
@@ -62,6 +70,7 @@ def main():
             print("Quitting...")
             break
 
+        #If user doesn't type add/complete/remove/reset/quit, then they get an error.
         else:
             print("Invalid choice. Please try again.")
 
